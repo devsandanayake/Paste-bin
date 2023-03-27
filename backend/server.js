84cd58bf330587e5931540c5c5104d731d0e6cb5
devsandanayake <devinsandanayake49@gmail.com>
2023-03-28 00:38:11 +0530

    server add

---
+++ backend/server.js
@@ -0,0 +1,54 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+const  cors = require('cors');
+const app = express();
+
+
+const PORT = process.env.PORT || 8080;
+//import routes
+const postBin = require('./routes/postD');
+ 
+ 
+ 
+
+
+
+//app middelware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.json());
+app.use(cors());
+
+
+
+//routes middelware
+app.use(postBin);
+
+
+
+
+app.get("/",(req,res)=>{
+    res.send("upload file")
+})
+
+ 
+const DB_URL='mongodb+srv://twg:pastebin@mernapp.1zdu8uy.mongodb.net/mernCrud?retryWrites=true&w=majority'
+mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', true);
+
+mongoose.connect(DB_URL)
+.then(()=>{
+    console.log('DB Connected');
+})
+.catch((err)=> console.log('DB connection error',err));
+
+
+app.listen(PORT,()=>{
+    console.log(`App is running on ${PORT}`);
+})
+
+
+
+
+
